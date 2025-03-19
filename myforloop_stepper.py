357b433ca2dd052acf6749cb8e5447c80bb40e9a
nayanprakash06 <124758491+nayanprakash06@users.noreply.github.com>
2025-03-19 22:11:08 +0530

    Update myforloop_stepper.py
    Changing the step pattern

--- myforloop_stepper.py
+++ myforloop_stepper.py
@@ -10,7 +10,7 @@ IN4 = Pin(15,Pin.OUT)
 
 pins = [IN1,IN2,IN3,IN4]
 steps = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
-
+step_2 = [[1,1,0,0],[0,1,1,0],[0,0,1,1],[1,0,0,1]]
 while True :
     for step in steps :
         for pin in range(len(pins)):
