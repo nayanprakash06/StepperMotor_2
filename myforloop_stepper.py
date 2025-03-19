1b72b99b5146e1500da04d0e329be6325ce4098a
nayan0621 <nayanprakash06@gmail.com>
2025-03-19 22:05:14 +0530

    Add files via upload

---
+++ myforloop_stepper.py
@@ -0,0 +1,18 @@
+# Write your code here :-)
+
+from machine import Pin
+from time import sleep
+
+IN1 = Pin(12,Pin.OUT)
+IN2 = Pin(13,Pin.OUT)
+IN3 = Pin(14,Pin.OUT)
+IN4 = Pin(15,Pin.OUT)
+
+pins = [IN1,IN2,IN3,IN4]
+steps = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+
+while True :
+    for step in steps :
+        for pin in range(len(pins)):
+            pins[pin].value(step[pin])
+            sleep(0.001)
