d650140afa0936f9da35921ccf8cd6fd57c31afa
nayanprakash06 <124758491+nayanprakash06@users.noreply.github.com>
2025-03-19 22:24:46 +0530

    Update myforloop_stepper.py

--- myforloop_stepper.py
+++ myforloop_stepper.py
@@ -10,9 +10,9 @@ IN4 = Pin(15,Pin.OUT)
 
 pins = [IN1,IN2,IN3,IN4]
 steps = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
-step_2 = [[1,1,0,0],[0,1,1,0],[0,0,1,1],[1,0,0,1]]
+step_3 = [[1,1,0,0],[0,1,1,0],[0,0,1,1],[1,0,0,1]]
 while True :
-    for step in step_2 :
+    for step in step_3 :
         for pin in range(len(pins)):
             pins[pin].value(step[pin])
             sleep(0.001)
